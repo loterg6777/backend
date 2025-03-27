b106f95aecfd42a127c23358f76a164609f3c0d9
loterg6777 <loterg6@gmail.com>
2025-03-27 03:15:54 -0300

    v1

---
+++ README.md
@@ -0,0 +1,15 @@
+# backendAPI
+
+### BACKEND API
+
+# UTILIZAÇÃO
+
+Express.js
+bodyParser.json
+Cors
+MongoDB
+Mongoose (Conexão com MongoDB) via Node
+
+# Sistema de Usuarios
+
+# Autenticação via JWT e Middleware
---
+++ modules/users/model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = {
+
+    usuario: String,
+    senha: String,
+    codigo: String,
+    tipo: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    criado: {
+        type: Date,
+        default: Date.now()
+    }
+}
+
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = UserModel
